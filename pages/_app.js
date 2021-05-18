7878664484e3d6db655bf591a1846bcf179bcf27
Matheus <matheus.olivieri@yahoo.com>
2021-05-18 20:08:51 -0300

    _app cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,50 @@
-import App from "next/app";
 import Head from "next/head";
-import "../styles/globals.css";
+import { ThemeProvider, DefaultTheme, createGlobalStyle } from "styled-components";
+import usePeristedState from "../utils/usePersistedState";
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <>
-        <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-            rel="stylesheet"
-          />
-          <link href="https://fonts.googleapis.com/css2?family=Harmattan:wght@400;700&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet" />
-        </Head>
-        <Component {...pageProps} />
-      </>
-    );
-  }
+import dark from "../styles/themes/dark";
+import light from "../styles/themes/light";
+
+const GlobalStyle = createGlobalStyle`
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: "Work Sans", sans-serif;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+`;
+
+export default function App({ Component, pageProps }) {
+  const [theme, setTheme] = usePeristedState("theme", dark);
+
+  const toggleTheme = () => {
+    setTheme(theme.name === "dark" ? light : dark);
+  };
+
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link href="https://fonts.googleapis.com/css2?family=Harmattan:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet" />
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} toggleTheme={toggleTheme} />
+      </ThemeProvider>
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import { ThemeProvider, DefaultTheme } from 'styled-components';
-import usePeristedState from '../utils/usePersistedState'
-
-import styles from '../styles/Home.module.css'
-import dark from "../styles/themes/dark"
-import light from "../styles/themes/light"
+import styles from '../styles/Home.module.scss'
 
 //COMPONENTS
 import Header from '../Components/Header'
 
-export default function Home() {
-  const [theme, setTheme] = usePeristedState<DefaultTheme>('theme', dark)
-
-  const toggleTheme = () => {
-    setTheme(theme.name === "dark" ? light : dark)
-  }
-
-
+export default function Home({toggleTheme}) {
   return (
-    <ThemeProvider theme={theme}>
       <div className={styles.container}>
         <Header toggleTheme={toggleTheme}/>
         <h1>MATHEUS OLIVIERI</h1>
       </div>
-    </ThemeProvider>
   )
 }
