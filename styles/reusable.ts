21d39e01077058fb29d10a09a9ca958084c4af62
Matheus <matheus.olivieri@yahoo.com>
2021-07-28 14:55:13 -0300

    page 1 and 2 responsive

--- styles/reusable.ts
+++ styles/reusable.ts
@@ -1,7 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const FlexCenter = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: 1200px) {
+    ///medium pc screens
+  }
+
+  @media (max-width: 992px) {
+    /// small pc screens
+  }
+  @media (max-width: 768px) {
+    ///tablets
+  }
+
+  @media (max-width: 576px) {
+    ///mobiles
+  }
+`;
